1ff342b60720dd6680548f7d87bc7799f3cac2f9
ggisel <giselle.d.isola@gmail.com>
2023-04-03 21:12:42 -0300

    asd

--- AppStreamlit.py
+++ AppStreamlit.py
@@ -1,38 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# importamos la libreria de streamlit para la interfaz de usuario
 import streamlit as st
-from analisis_crediticio import *
+import pickle
 import pandas as pd
-
-st.set_page_config(page_title="Analisis crediticio con ML", layout="wide")
-st.image('https://imageup.me/images/1bc21b54-ecb1-4fde-a7c6-582acb7cc4a8.png')
-
-modelo_RF = cargar_modelo('ML_RFmodel4_Analisis_crediticio.pkl')
-modelo_LR = cargar_modelo('ML_LRmodel2_Analisis_crediticio.pkl')
-
-st.subheader('Ajuste en la prediccion')
-option = ['Regresion logistica', 'Random forest']
-# escoger el modelo preferido
-model = st.selectbox('Que modelo prefiere usar?', option)
-
-uploaded_file = st.file_uploader("Seleccione el archivo de prueba", type=['csv'], key="1")
+from io import StringIO
 
 
+# In[3]:
 
-if uploaded_file is not None:
-    st.subheader('Evaluacion por Archivos')
-    data = pd.read_csv(uploaded_file)
-    st.dataframe(data)
-    if st.button('RUN'):
+
+filename = 'ML_RFmodel4_Analisis_crediticio'
+
+filename2 = 'ML_LRmodel2_Analisis_crediticio'
+# In[4]:
+
+
+modelo_RF4= pickle.load(open(filename,'rb'))
+modelo_LR2= pickle.load(open(filename,'rb'))
+
+# In[5]:
+
+
+modelo_RF4.get_params()
+
+
+# In[6]:
+
+
+#Creamos la funcion para clasificar a los clientes segun lo que nos devuelva el modelo
+def classify(num):
+    if num == 0:
+        return 'tipo 0'
+    elif num == 1:
+        return 'tipo 1'
+    else:
+        return 'tipo 2'
+
+
+# In[1]:
+
+
+def main():
+    #titulo
+    st.image('https://imageup.me/images/1bc21b54-ecb1-4fde-a7c6-582acb7cc4a8.png')
+    
+    #titulo de sidebar
+    st.sidebar.header('Selección del Modelo')
+
+    
+    st.sidebar.subheader('')
+
+    #escoger el modelo preferido
+    option = ['Regresion logistica', 'Random forest']
+    model = st.sidebar.selectbox('Que modelo prefiere usar?', option)
+
+   
+            
+    st.subheader('Evaluar Archivo')
+    
+    uploaded_file = st.file_uploader("Seleccione el archivo de prueba")
+    if uploaded_file is not None:
         # Convierte el archivo cargado a un DataFrame
+        data2 = pd.read_csv(uploaded_file)
+        # st.write(data)
+        st.subheader('Resultados:')
+        st.caption('0 si es viable otorgar el prestamo')
+        st.caption('0 si debe ser negado ')
         if model == 'Regresion logistica':
-            prediccion = predecir(modelo_LR, data)
-            df_resultado = agregar_predicciones(data, prediccion)
-            st.subheader('Evaluacion por Archivos')
-            st.dataframe(df_resultado)
+            
+            predicciones= modelo_LR2.predict(data2)
+           
         else:
-            prediccion = predecir(modelo_LR, data)
-            df_resultado = agregar_predicciones(data, prediccion)
-            st.subheader('Evaluacion por Archivos')
-            st.dataframe(df_resultado)
+            
+            predicciones= modelo_RF4.predict(data2)
+        
+        st.write(predicciones)
+    
+
+        
+       
+       
+
+
+
+# In[8]:
+
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
 
 
 
